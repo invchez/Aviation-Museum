a510a78a5a263395c3a91242d0b35bd90c3f4a3f
Moddwyn <moddwyn@gmail.com>
2026-03-29 13:00:57 -0700

    Update SceneSwitcher.cs

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -4,57 +4,29 @@ using System.Collections.Generic;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public Animator panel;
 
-    public GameObject Panel;
-    public List <Animator> ButtonAnimators;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Panel.GetComponent<AnimationEnd>().AnimationEndedEvent += DeactivatePanel;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        panel.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")){
-            Panel.SetActive(true);
-            Panel.GetComponent<Animator>().Play("SceneSwitcherFadeIn");
-            foreach (Animator animator in ButtonAnimators)
-            {
-                animator.Play("SceneSwitcherButtonFadeIn");
-            }
+        if (other.CompareTag("Player"))
+        {
+            panel.gameObject.SetActive(true);
+            panel.GetComponent<Animator>().Play("SceneSwitcherFadeIn");
         }
-            
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")){
-            Panel.GetComponent<Animator>().Play("SceneSwitcherFadeOut");
-            foreach (Animator animator in ButtonAnimators)
-            {
-                animator.Play("SceneSwitcherButtonFadeOut");
-            }
-
-            
+        if (other.CompareTag("Player"))
+        {
+            panel.GetComponent<Animator>().Play("SceneSwitcherFadeOut");
         }
-            
-    }
 
-    public void SwitchScenes(string Scene)
-    {
-        GameObject.FindWithTag("transition").GetComponent<Transition>().FadeIn(Scene);
-    }
-
-    void DeactivatePanel()
-    {
-        Panel.SetActive(false);
     }
 }
